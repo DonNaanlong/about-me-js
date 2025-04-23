d6a18c7e6748f56778ce7003748f224d02f2b2af
deshinaanlongjoshua <deshinaanlong@gmail.com>
2025-04-23 15:49:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+    let app = document.getElementById("app")
+
+    let container = document.createElement("h1");
+    container.id ="body"
+    title.innerText = "Hello there!, here is a little information about me"
+    hello.appendchild(container)
+
+    let intro = document.createElement("p");
+    intro.innerText = "My name is <strong>DESHI NAANLONG JOSHUA</strong> AKA (DON), i hail form kerang, Mangu LGA <em>plateau state</em>"
+    little.appendChild(intro);
+    
+    let intro = document.createElement("h2");
+    bio.innerText = ""
+    .appendChild(intro);
+    
